6fd05dab8214e4749641d4067db1b808d6bceeb7
Chaoran Xie <cxie@hubspot.com>
2013-08-16 01:38:48 -0400

    change app name

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Myapp::Application.config.secret_key_base = '88a70beea6582e722a20b5615524633319bacb2ad8a64cf95966d63c169e201620a4de5e3396456293d1f68a5507216abdc54f181fc751d98456c042e001f697'
+ChaoIO::Application.config.secret_key_base = '88a70beea6582e722a20b5615524633319bacb2ad8a64cf95966d63c169e201620a4de5e3396456293d1f68a5507216abdc54f181fc751d98456c042e001f697'
